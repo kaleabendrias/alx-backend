a28ead06bf941b07afd2c9e0ea8f1ae27ee522bb
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-28 21:21:16 +0300

    Add deletion-resilient hypermedia pagination implementation

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """func hyper_index"""
+        assert index is None or (isinstance(index, int)
+                                 and 0 <= index < len(self.__indexed_dataset))
+        next_index = index + page_size if index is not None else 0
+        data = []
+
+        if index is not None:
+            for i in range(index,
+                           min(index + page_size,
+                               len(self.__indexed_dataset))):
+                if i in self.__indexed_dataset:
+                    data.append(self.__indexed_dataset[i])
+
+        return {
+            'index': index,
+            'next_index': next_index if next_index < len
+            (self.__indexed_dataset) else None,
+            'page_size': page_size,
+            'data': data
+        }
