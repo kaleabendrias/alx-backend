5f1c69c76553f33e693b80a5af957c53652bc3fc
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-31 10:23:44 +0300

    Refactor put method in LRUCache class

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -12,15 +12,16 @@ class LRUCache(base_caching):
 
     def put(self, key, item):
         """put method"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                discarded_key = self.lru_list.pop(0)
-                del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
-            self.cache_data[key] = item
-            if key in self.lru_list:
-                self.lru_list.remove(key)
-            self.lru_list.append(key)
+        if key is None and item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.lru_list.pop(0)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+        self.cache_data[key] = item
+        if key in self.lru_list:
+            self.lru_list.remove(key)
+        self.lru_list.append(key)
 
     def get(self, key):
         """get method"""
