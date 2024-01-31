0a523ab4087d03ac859043603289c253bb0fa7bb
kaleabendrias <kaleabendrias1212@gmail.com>
2024-01-31 10:08:51 +0300

    Add LRU cache implementation

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""lru cache"""
+base_caching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(base_caching):
+    """class lru caching"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.lru_list = []
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discarded_key = self.lru_list.pop(0)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+            if key in self.lru_list:
+                self.lru_list.remove(key)
+            self.lru_list.append(key)
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            if key in self.lru_list:
+                self.lru_list.remove(key)
+            self.lru_list.append(key)
+            return self.cache_data[key]
+        return None
