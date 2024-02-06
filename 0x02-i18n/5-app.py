f7784b88827f92306fff316e5702129cb825173f
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-07 00:22:18 +0300

    repair

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,16 +4,15 @@ from flask import Flask, request, render_template, g
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
 class Config:
     """config available languages in our app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
+babel = Babel(app)
 
 app.config.from_object(Config)
 
