530b50ce4352af2caf1033d7f1c945084954a2a2
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-06 19:18:44 +0300

    Add Babel support to Flask app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""instantiate the Babel object in your app"""
+from flask import Flask
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config available languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+if __name__ == '__main__':
+    app.run()
