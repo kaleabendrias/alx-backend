1fafaad918399a3dbf9cf1026384ab12defc1786
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-06 22:31:33 +0300

    Add internationalization support to the Flask app

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Use request.accept_languages to determine the best match"""
+from flask import Flask, request, render_template, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config available languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    login_as = request.args.get('login_as')
+    if login_as and int(login_as) in users:
+        return users[int(login_as)]
+    else:
+        return None
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+@babel.localeselector
+def get_locale():
+    """ detect if the incoming request contains locale argument"""
+    if 'locale' in request.args:
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    """renders 4-index.html"""
+    return render_template("5-index.html", user=g.user)
+
+
+if __name__ == '__main__':
+    app.run()
